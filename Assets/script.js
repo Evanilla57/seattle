aa5320ea782004d408238243fcfde8f7519f090f
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-20 18:16:44 -0700

    added function to prompt user to select password length

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "qwertyuiopasdfghjklzxcvbnm"
-var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var numbers = "1234567890"
-var specialChar = "!@#$%^&*()"
+var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var numbers = "1234567890";
+var specialChar = "!@#$%^&*()";
+var possibleChoices = [];
+
+function generatePassword() {
+window.prompt("Select how many characters you wish your password to be. You may choose between 8 and 128 characters in length.")
+};
 
 // Write password to the #password input
 function writePassword() {
